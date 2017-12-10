e30a456e14483bc744e586c8972424454be15067
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-12-10 19:21:54 +0100

    Remove unnecessary content from template

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './assets/scss/main.scss';
 
 class App extends Component {
@@ -7,7 +6,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to s</h1>
         </header>
         <p className="App-intro">
