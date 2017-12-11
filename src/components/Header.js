adb718f7273284eb26f1f8b897accc9c2a790865
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-12-11 22:06:57 +0100

    Prepare header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,19 @@ class Header extends Component {
   render() {
     return (
       <header className="main-header">
-
+        <div className="logo">
+          <h1>Groupeé</h1>
+        </div>
+        <div>
+          <ul>
+            <li><input type="text" placeholder="Username"/></li>
+            <li><input type="password" placeholder="Password"/></li>
+            <li><button>Sign In</button></li>
+            <li>
+              <a href="">new? register now</a>
+            </li>
+          </ul>
+        </div>
       </header>
     );
   }
