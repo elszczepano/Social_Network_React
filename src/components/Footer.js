b6daeb9d2b95caaec4fb12850fc63ec543ec4f15
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-12-14 17:27:26 +0100

    Add basic footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react';
 import '../assets/scss/main.scss';
 import '../assets/scss/footer.scss';
 
+function getCurrentYear(date) {
+  return (new Date()).getFullYear();
+}
+
+
 class Footer extends Component {
   render() {
     return (
-      <div className="main-footer"></div>
+      <div className="main-footer">
+      <p>Groupeé <sup>&copy;</sup> 2017 - { getCurrentYear() }</p>
+      <ul>
+        <li><a href="">About</a></li>
+        <li><a href="">FAQ</a></li>
+        <li><a href="">Support</a></li>
+        <li><a href="">Contact</a></li>
+      </ul>
+      </div>
     );
   }
 }
