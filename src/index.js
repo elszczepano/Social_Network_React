adbe2ad7ff94d92661cc4da923c1603ebb635332
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-12-17 14:03:24 +0100

    Responsive landing page

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import Footer from './components/Footer';
 import Landing from './components/Landing';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Footer />, document.getElementById('footer'));
 ReactDOM.render(<Landing />, document.getElementById('root'));
+ReactDOM.render(<Footer />, document.getElementById('footer'));
 registerServiceWorker();
