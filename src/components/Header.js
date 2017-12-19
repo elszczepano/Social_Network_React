54db2b762a2601cecafe2edf9400a6140e4f9553
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-12-19 16:46:54 +0100

    Default header component

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import '../assets/scss/main.scss';
+import '../assets/scss/header.scss';
+
+class Header extends Component {
+  render() {
+    return (
+        <header className="main-page-header">
+          <div className="logo">
+            <h1>Groupeé</h1>
+          </div>
+          <div>
+            <ul className="user-icons">
+              <li><span class="fa fa-user" aria-hidden="true"></span></li>
+              <li><span class="fa fa-comments active" aria-hidden="true"></span></li>
+              <li><span class="fa fa-bell-o" aria-hidden="true"></span></li>
+              <li><button>Logout</button></li>
+            </ul>
+          </div>
+        </header>
+    );
+  }
+}
+
+export default Header;
