c75d6b82ca57565a488996f6cc499aa568e330ca
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-12-24 13:46:03 +0100

    add names and labels to inputs

--- src/components/Register.js
+++ src/components/Register.js
@@ -10,30 +10,32 @@ class Register extends Component {
           <div className="modal-header">
             <span onClick={this.props.registerVisibility} className="fa fa-times" aria-hidden="true"></span>
           </div>
-          <div className="modal-body">
-            <h2>Create an account</h2>
-            <label>Name</label>
-            <input type="text"/>
-            <label>Surname</label>
-            <input type="text"/>
-            <label>E-mail</label>
-            <input type="email"/>
-            <label htmlFor="birthdate">Birth date</label>
-            <input name="birthdate" type="date"/>
-            <label>Password</label>
-            <input type="password"/>
-            <label>Confirm password</label>
-            <input type="password"/>
-            <div className="pretty p-default">
-              <input type="checkbox" />
-              <div className="state p-primary-o">
-               <label>Accept <a href="">the terms</a></label>
+          <form action="" method="post">
+            <div className="modal-body">
+              <h2>Create an account</h2>
+              <label htmlFor="firstname">Name</label>
+              <input name="firstname" type="text"/>
+              <label htmlFor="lastname">Surname</label>
+              <input name="lastname" type="text"/>
+              <label htmlFor="email">E-mail</label>
+              <input name="email" type="email"/>
+              <label htmlFor="birthdate">Birth date</label>
+              <input name="birthdate" type="date"/>
+              <label htmlFor="password">Password</label>
+              <input name="password" type="password"/>
+              <label htmlFor="confirmPassword">Confirm password</label>
+              <input name="confirmPassword" type="password"/>
+              <div className="pretty p-default">
+                <input type="checkbox" />
+                <div className="state p-primary-o">
+                 <label>Accept <a href="">the terms</a></label>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="modal-footer">
-            <button type="submit">Register</button>
-          </div>
+            <div className="modal-footer">
+              <button type="submit">Register</button>
+            </div>
+          </form>
         </div>
       </div>
     );
