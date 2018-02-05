19ee9c98c720143419c8ddb6a9cebf38e2f85c9d
elszczepano <szczepaniak.dominik98@gmail.com>
2018-02-05 15:20:24 +0100

    Minor changes

--- src/components/NotificationBox.js
+++ src/components/NotificationBox.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 import Notification from './Notification';
+import Message from './Message';
 import '../assets/scss/main.scss';
 import '../assets/scss/notification.scss';
 
 class NotificationBox extends Component {
-  
-  unreaded = 2;
+  constructor(){
+    super();
+    this.state = {
+      messages: [
+        {
+          message: 'test',
+          isRead: false
+        }
+      ]
+    }
+  }
+  unread = 2;
   render() {
     return (
       <div className="notification-box">
         <header>
-          <div>Notifications({this.unreaded})</div>
+          <div>Notifications({this.unread})</div>
           <div><span className="fa fa-caret-up"></span></div>
         </header>
         <section>
