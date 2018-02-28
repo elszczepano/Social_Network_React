c14e2966059efe5fa8b5df7ea8400fa221a03195
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-02-28 18:12:04 +0100

    Add different passwords box, fetching data from form, template fixes

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,8 +1,37 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import API from '../api.js';
 import '../assets/scss/main.scss';
 import '../assets/scss/register.scss';
 
 class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {name: "", surname: "", email: "", birth_date: "", password: "", confirmPassword: "", errMessage: ""};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleRegister = this.handleRegister.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({[event.target.id]: event.target.value});
+  }
+  handleRegister(event) {
+
+    if(this.state.password!=this.state.confirmPassword) {
+      this.setState({errMessage: "Passwords are different"});
+      event.preventDefault();
+    }
+    const user = {
+        name: this.state.name,
+        surname: this.state.surname,
+        email: this.state.email,
+        birthDate: this.state.birthDate,
+        password: this.state.password
+    };
+    //TODO axios post request
+  }
+
   render() {
     return (
       <div className="modal-container">
@@ -13,27 +42,30 @@ class Register extends Component {
           <form action="" method="post">
             <div className="modal-body">
               <h2>Create an account</h2>
-              <label htmlFor="firstname">Name</label>
-              <input name="firstname" type="text" required/>
-              <label htmlFor="lastname">Surname</label>
-              <input name="lastname" type="text" required/>
-              <label htmlFor="email">E-mail</label>
-              <input name="email" type="email" required/>
-              <label htmlFor="birthdate">Birth date</label>
-              <input name="birthdate" type="date" required/>
-              <label htmlFor="password">Password</label>
-              <input name="password" type="password" required/>
-              <label htmlFor="confirmPassword">Confirm password</label>
-              <input name="confirmPassword" type="password" required/>
+                <label htmlFor="firstname">Name<span className="warning-marker">*</span></label>
+              <input id="name" type="text" value={this.state.name} onChange={this.handleChange} required/>
+                <label htmlFor="lastname">Surname</label>
+              <input id="surname" type="text" value={this.state.surname} onChange={this.handleChange}/>
+                <label htmlFor="email">E-mail<span className="warning-marker">*</span></label>
+              <input id="email" type="email" value={this.state.email} onChange={this.handleChange} required/>
+                <label htmlFor="birthDate">Birth date</label>
+              <input id="birthDate" type="date" value={this.state.birthDate} onChange={this.handleChange}/>
+                <label htmlFor="password">Password<span className="warning-marker">*</span></label>
+              <input id="password" type="password" value={this.state.password} onChange={this.handleChange} required/>
+                <label htmlFor="confirmPassword">Confirm password<span className="warning-marker">*</span></label>
+              <input id="confirmPassword" type="password" value={this.state.confirmPassword} onChange={this.handleChange} required/>
+              <div className="warning-marker register-error-box">
+                {this.state.errMessage}
+              </div>
               <div className="pretty p-default">
-                <input name="terms" type="checkbox" required/>
+                <input id="terms" type="checkbox" required/>
                 <div className="state p-primary-o">
-                  <label htmlFor="terms">Accept the terms of service</label>
+                  <label htmlFor="terms">Accept the terms of service<span className="warning-marker">*</span></label>
                 </div>
               </div>
             </div>
             <div className="modal-footer">
-              <button type="submit">Register</button>
+              <button type="submit" onClick={this.handleRegister}>Register</button>
             </div>
           </form>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-//import Landing from './components/Landing';
+import Landing from './components/Landing';
 //import UserAccount from './components/UserAccount';
 //import NotificationBox from './components/NotificationBox';
-import NewsFeed from './components/NewsFeed';
+//import NewsFeed from './components/NewsFeed';
 //import GroupAdmin from './components/GroupAdmin';
 //import EditAccountDetails from './components/EditAccountDetails';
 import registerServiceWorker from './registerServiceWorker';
 
-//ReactDOM.render(<Landing />, document.getElementById('root'));
+ReactDOM.render(<Landing />, document.getElementById('root'));
 //ReactDOM.render(<UserAccount />, document.getElementById('root'));
-ReactDOM.render(<NewsFeed />, document.getElementById('root'));
+//ReactDOM.render(<NewsFeed />, document.getElementById('root'));
 //ReactDOM.render(<NotificationBox />, document.getElementById('root'));
 //ReactDOM.render(<GroupAdmin />, document.getElementById('root'));
 //ReactDOM.render(<EditAccountDetails />, document.getElementById('root'));
