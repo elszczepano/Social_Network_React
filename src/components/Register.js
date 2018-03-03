986d201df2e62e79266abe11f4d175a779287e29
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-03-03 15:19:11 +0100

    Improve error handling, correct messages

--- src/components/Register.js
+++ src/components/Register.js
@@ -7,24 +7,44 @@ import '../assets/scss/register.scss';
 class Register extends Component {
   constructor(props) {
     super(props);
-    this.state = { errMessage: []};
+    this.state = { errMessage: [], terms: false, registered: false, name: "", email: "", password: "", confirmPassword: ""};
 
     this.handleChange = this.handleChange.bind(this);
     this.handleRegister = this.handleRegister.bind(this);
+    this.handleAcceptTerms = this.handleAcceptTerms.bind(this);
   }
 
   handleChange(event) {
     this.setState({[event.target.id]: event.target.value});
   }
+
+  handleAcceptTerms() {
+    this.setState({
+      terms: !this.state.terms
+    });
+  }
+
   handleRegister(event) {
+    this.setState({
+      registered: false
+    });
+
     let message = [];
+
+    this.setState({errMessage: message});
+
     if(this.state.password!==this.state.confirmPassword) {
       message.push("Passwords are different");
+    }
+
+    if(!this.state.terms) {
+      message.push("Accept the terms");
+    }
+
+    if(message.length) {
       this.setState({errMessage: message});
-      event.preventDefault();
       return;
     }
-    else this.setState({errMessage: []});
 
     API.post('/register',{
       name: this.state.name,
@@ -32,7 +52,11 @@ class Register extends Component {
       password: this.state.password
     })
     .then(response => {
-      console.log(response)
+      this.setState({
+        registered: !this.state.registered
+      });
+      message.push("User created succesfully. Now you can sign in");
+      this.setState({errMessage: message});
     })
     .catch(error => {
       const response = error.response['data']['message'];
@@ -40,7 +64,6 @@ class Register extends Component {
         message.push(response[key].toString());
       })
       this.setState({errMessage: message});
-      console.log(this.state.errMessage);
     });
   }
 
@@ -54,22 +77,22 @@ class Register extends Component {
             <div className="modal-body">
               <h2>Create an account</h2>
                 <label htmlFor="firstname">Name<span className="warning-marker">*</span></label>
-              <input id="name" type="text" value={this.state.name} onChange={this.handleChange} required/>
+              <input id="name" type="text" value={this.state.name} onChange={this.handleChange}/>
                 <label htmlFor="email">E-mail<span className="warning-marker">*</span></label>
-              <input id="email" type="email" value={this.state.email} onChange={this.handleChange} required/>
+              <input id="email" type="email" value={this.state.email} onChange={this.handleChange}/>
                 <label htmlFor="password">Password<span className="warning-marker">*</span></label>
-              <input id="password" type="password" value={this.state.password} onChange={this.handleChange} required/>
+              <input id="password" type="password" value={this.state.password} onChange={this.handleChange}/>
                 <label htmlFor="confirmPassword">Confirm password<span className="warning-marker">*</span></label>
-              <input id="confirmPassword" type="password" value={this.state.confirmPassword} onChange={this.handleChange} required/>
-              <div  className="register-message-box">
+              <input id="confirmPassword" type="password" value={this.state.confirmPassword} onChange={this.handleChange}/>
+              <div className="register-message-box">
               {
                 this.state.errMessage.map((value) =>
-                  <p className="warning-marker" key={value.toString()}>{value}</p>
+                  <p className= { this.state.registered ? "success-marker" : "warning-marker" } key={value.toString()}>{value}</p>
                 )
               }
               </div>
               <div className="pretty p-default">
-                <input id="terms" type="checkbox" required/>
+                <input id="terms" type="checkbox" onChange={this.handleAcceptTerms}/>
                 <div className="state p-primary-o">
                   <label htmlFor="terms">Accept the terms of service<span className="warning-marker">*</span></label>
                 </div>
