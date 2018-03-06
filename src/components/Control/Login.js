62163bb85f603bb884361d00ec4528592de0d01c
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-03-06 15:23:12 +0100

    Prepare login file and request

--- src/components/Control/Login.js
+++ src/components/Control/Login.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react';
+import API from '../../api.js';
 import '../../assets/scss/main.scss';
 import '../../assets/scss/header/login.scss';
 
 class Login extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {email: "", password: "", errMessage: ""};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({[event.target.id]: event.target.value});
+  }
+
+  handleLogin(event) {
+    API.post('/login', {
+    email: this.state.email,
+    password: this.state.password
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error.response);
+  });
+  }
+
+
   render() {
     return (
       <div className="login-box">
         <ul>
-          <li><input type="text" placeholder="Username"/></li>
-          <li><input type="password" placeholder="Password"/></li>
-          <li><button>Sign In</button></li>
+          <li className="warning-marker">{this.state.errMessage}</li>
+          <li><input id="email" value={this.state.email} onChange={this.handleChange} type="text" placeholder="Email"/></li>
+          <li><input id="password" value={this.state.password} onChange={this.handleChange} type="password" placeholder="Password"/></li>
+          <li><button onClick={this.handleLogin}>Sign In</button></li>
         </ul>
       </div>
     );
