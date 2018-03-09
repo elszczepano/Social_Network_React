37be09523d9f5d835a75e0662cc6f706ca4daa57
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-03-09 13:56:10 +0100

    Clear index file from redundant components

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import Landing from './components/Containers/Landing';
-//import UserAccount from './components/UserAccount';
-//import NotificationBox from './components/NotificationBox';
-//import NewsFeed from './components/NewsFeed';
-//import GroupAdmin from './components/GroupAdmin';
-//import EditAccountDetails from './components/EditAccountDetails';
+import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Landing />, document.getElementById('root'));
-//ReactDOM.render(<UserAccount />, document.getElementById('root'));
-//ReactDOM.render(<NewsFeed />, document.getElementById('root'));
-//ReactDOM.render(<NotificationBox />, document.getElementById('root'));
-//ReactDOM.render(<GroupAdmin />, document.getElementById('root'));
-//ReactDOM.render(<EditAccountDetails />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
