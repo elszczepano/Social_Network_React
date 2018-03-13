f6d04e63a9b20092683fc6cfbda62642b47548b9
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-03-13 18:03:33 +0100

    Fetch data of current logged user

--- src/components/View/UserShortcut.js
+++ src/components/View/UserShortcut.js
@@ -1,40 +1,46 @@
 import React, { Component } from 'react';
+import API from '../../api.js';
 import '../../assets/scss/main.scss';
 import '../../assets/scss/user/usershortcut.scss';
 
 class UserShortcut extends Component {
-  meShortcut = {
-    firstName: 'John',
-    lastName: 'Doe',
-    avatar: 'https://avatarfiles.alphacoders.com/855/85557.png',
-    groups: {
-      'IT Devs': 'rocket',
-      'buy/sell Poland': 'camera-retro',
-      'Memes': 'image',
-      'Janusz Pol - workmates': 'money',
-      'Fishing fanatics': 'ship',
-      'Star Wars fans': 'rebel'
-    }
+  constructor(props) {
+    super(props);
+    this.state = {name: "", surname: "", avatar: ""};
+  }
+
+  componentWillMount() {
+    API.get('/me')
+    .then(response => {
+      response = response['data'];
+      this.setState({
+        name: response['name'],
+        surname: response['surname'],
+      });
+      if(response['avatar']) {
+        this.setState({avatar: response['avatar']});
+      }
+      else {
+        this.setState({avatar: '../../assets/images/avatar-15777909_1280.png'});
+      }
+    })
+    .catch(error => {
+      const response = error.response['data']['message'];
+      console.log(response);
+    });
   }
   render() {
     return (
       <aside className="user-shortcuts">
         <div className="account-shortcut">
-          <img src={this.meShortcut.avatar} alt={`${this.meShortcut.firstName} ${this.meShortcut.lastName} avatar`}/>
-          <h3><a className="text-marker" href="">{this.meShortcut.firstName} {this.meShortcut.lastName}</a></h3>
+          <img src={this.state.avatar} alt={`${this.state.name} ${this.state.surname} avatar`}/>
+          <h3><a className="text-marker" href="">{this.state.name} {this.state.surname}</a></h3>
         </div>
         <div className="notifications-shortcut">
           <h4 className="text-marker"><span className="fa fa-bell" aria-hidden="true"></span> Notifications</h4>
         </div>
         <div className="groups-shortcut">
           <h4><span className="fa fa-users" aria-hidden="true"></span> Groups</h4>
-          {
-            Object.keys(this.meShortcut.groups).map ((value) =>
-            <ul className="default-group-list" key={value.toString()}>
-              <li><a href=""><span className={`fa fa-${this.meShortcut.groups[value]}`}></span> {value}</a></li>
-            </ul>
-            )
-          }
         </div>
       </aside>
     );
