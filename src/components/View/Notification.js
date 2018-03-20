21dbbc16764ee9e6d13ea6670288ba7c71542be0
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-03-20 14:05:14 +0100

    Add proptypes and dynamic displaying notifications

--- src/components/View/Notification.js
+++ src/components/View/Notification.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import '../../assets/scss/main.scss';
 import '../../assets/scss/user/notification.scss';
 
@@ -9,12 +10,16 @@ class Notification extends Component {
   render() {
     return (
       <div className="notification">
-        <p>Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.</p>
+        <p>{this.props.content.content}</p>
         <span className="fa fa-check"></span>
         <span className="fa fa-times"></span>
       </div>
     );
   }
+}
+
+Notification.propTypes = {
+  content: PropTypes.object
 }
 
 export default Notification;
