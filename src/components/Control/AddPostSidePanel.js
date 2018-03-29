428a39b42a1b65059879cef192a0e216d2cf95ff
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-03-29 18:46:10 +0200

    Create short add post template

---
+++ src/components/Control/AddPostSidePanel.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../../assets/scss/main.scss';
+import '../../assets/scss/sidepanel/addpost.scss';
+
+class AddPostSidePanel extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default AddPostSidePanel;
