dd6479fa20d1980053062d3cddbff32b5e8b1541
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-03-29 18:46:47 +0200

    Create short create group component

---
+++ src/components/Control/CreateGroupShort.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import API from '../../api.js';
+import '../../assets/scss/main.scss';
+import '../../assets/scss/sidepanel/creategroup.scss';
+
+class CreateGroupShort extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {icons: []};
+  }
+
+  componentWillMount() {
+    API.get('/icons', { 'headers': { 'Authorization': localStorage.getItem("token")} })
+    .then(response => {
+      response = response['data'];
+      response = response.map(icon => ({
+        id: icon.id,
+        name: icon.name
+      }));
+      this.setState({
+        icons: response
+      })
+    })
+  }
+  render() {
+    return (
+      <form className="instant-create-group">
+        <h3>Create group instantly</h3>
+        <input placeholder="Group name" type="text"/>
+        <div>
+          <label htmlFor="icon">Select icon:</label>
+          <select name="icon" id="">
+            {
+              this.state.icons.map((icon, index) => {
+                console.log(icon);
+                return <option key={index}>{icon.name}</option>
+              })
+            }
+          </select>
+        </div>
+        <div className="instant-create-group-submit">
+          <button type="submit">Create</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default CreateGroupShort;
