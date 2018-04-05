39a0722dc22e393d3fd8782792cab71c099e6784
elszczepano <szczepaniak.dominik98@gmail.com>
2018-04-05 13:41:13 +0200

    Fetch user groups

--- src/components/Control/AddPostSidePanel.js
+++ src/components/Control/AddPostSidePanel.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import API from '../../api.js';
 import { connect } from 'react-redux';
 import '../../assets/scss/main.scss';
 import '../../assets/scss/sidepanel.scss';
 
 class AddPostSidePanel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {groups: []};
+  }
+  componentWillMount() {
+      API.get(`/user/groups/${this.props.user.id}`, { 'headers': { 'Authorization': localStorage.getItem("token")} })
+      .then(response => {
+        response = response['data'];
+        response = response.map(group => ({
+          name: group.name,
+          id: group.icon.id
+        }));
+        this.setState({
+          groups: response
+        })
+      })
+    .catch(error => {
+      if(error.response) {
+        const response = error.response['data']['message'];
+        console.log(response);
+      }
+      else {
+        console.log(error);
+      }
+    });
+  }
   render() {
     return (
       <form className="instant-add-post">
@@ -12,7 +39,11 @@ class AddPostSidePanel extends Component {
         <div>
           <label htmlFor="group">Select group:</label>
           <select name="group">
-            <option value="">dupa</option>
+          {
+            this.state.groups.map((group) => {
+              return <option value={group.id} key={group.id}>{group.name}</option>
+            })
+          }
           </select>
         </div>
         <textarea rows="5" placeholder="Write something..."></textarea>
