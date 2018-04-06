a64966ab6f40d31fa590a6d18b8c09da51a4ec42
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-04-06 14:08:25 +0200

    Add creating post in group

--- src/components/Control/AddPostSidePanel.js
+++ src/components/Control/AddPostSidePanel.js
@@ -8,7 +8,7 @@ import '../../assets/scss/sidepanel.scss';
 class AddPostSidePanel extends Component {
   constructor(props) {
     super(props);
-    this.state = {groups: []};
+    this.state = {groups: [], group: 0, content: ""};
   }
   componentWillMount() {
       API.get(`/user/groups/${this.props.user.id}`, { 'headers': { 'Authorization': localStorage.getItem("token")} })
@@ -16,7 +16,7 @@ class AddPostSidePanel extends Component {
         response = response['data'];
         response = response.map(group => ({
           name: group.name,
-          id: group.icon.id
+          id: group.id
         }));
         this.setState({
           groups: response
@@ -32,13 +32,43 @@ class AddPostSidePanel extends Component {
       }
     });
   }
+
+  handleChange = (event) => {
+    this.setState({[event.target.id]: event.target.value});
+  }
+
+  addPost = (event) => {
+    event.preventDefault();
+
+    if(this.state.content&&this.state.group) {
+      API.post('/posts',
+      {
+        content: this.state.content,
+        group_id: this.state.group,
+        user_id: this.props.user.id
+      },
+      {
+        'headers': { 'Authorization': localStorage.getItem("token")}
+      })
+      .catch(error => {
+        if(error.response) {
+          const response = error.response['data']['message'];
+          console.log(response);
+        }
+        else {
+          console.log(error);
+        }
+      });
+    }
+  }
   render() {
     return (
       <form className="instant-add-post">
         <h3>Add post instantly</h3>
         <div>
           <label htmlFor="group">Select group:</label>
-          <select name="group">
+          <select id="group" onChange={this.handleChange} name="group">
+            <option value="0">Choose group</option>
           {
             this.state.groups.map((group) => {
               return <option value={group.id} key={group.id}>{group.name}</option>
@@ -46,9 +76,9 @@ class AddPostSidePanel extends Component {
           }
           </select>
         </div>
-        <textarea rows="5" placeholder="Write something..."></textarea>
+        <textarea id="content" onChange={this.handleChange} rows="5" placeholder="Write something..."></textarea>
         <div className="aside-button-submit">
-          <button type="submit">Add</button>
+          <button type="submit" onClick={this.addPost}>Add</button>
         </div>
       </form>
     );
