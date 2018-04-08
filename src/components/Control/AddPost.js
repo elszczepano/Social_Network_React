022ccd3804497518ab75c49270687b8e06b093ce
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-04-08 15:05:44 +0200

    Remove unnecessary code from component

--- src/components/Control/AddPost.js
+++ src/components/Control/AddPost.js
@@ -3,32 +3,14 @@ import '../../assets/scss/main.scss';
 import '../../assets/scss/post/addpost.scss';
 
 class AddPost extends Component {
-  meShortcut = {
-    groups: {
-      'IT Devs': 'rocket',
-      'buy/sell Poland': 'camera-retro',
-      'Memes': 'image',
-      'Janusz Pol - workmates': 'money',
-      'Fishing fanatics': 'ship',
-      'Star Wars fans': 'rebel'
-    }
-  }
   render() {
     return (
       <div className="add-post-wrapper">
-        <div>
         <h3>Write something:</h3>
-        <select name="" id="">
-        <option defaultValue disabled>Choose group</option>
-        {
-          Object.keys(this.meShortcut.groups).map ((value) =>
-          <option key={value.toString()} value={value}>{value}</option>
-          )
-        }
-        </select>
-        </div>
         <textarea name="" id="" rows="8" />
-        <button><span className="fa fa-pencil-square-o" aria-hidden="true"></span> Post</button>
+        <div>
+          <button><span className="fa fa-pencil-square-o" aria-hidden="true"></span> Post</button>
+        </div>
       </div>
     );
   }
