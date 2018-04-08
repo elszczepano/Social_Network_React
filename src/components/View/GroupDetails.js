7c30896c75f39bcabe702181a1c274fb66c0b732
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-04-08 15:05:22 +0200

    Add component styles and display data

--- src/components/View/GroupDetails.js
+++ src/components/View/GroupDetails.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../../assets/scss/main.scss';
+import '../../assets/scss/group/groupdetails.scss';
 
 const GroupDetails = (props) => (
-  <div>
-    <div>
+  <div className="group-details-wrapper">
+    <div className="group-background">
       <h3>{props.details.name}</h3>
     </div>
+    <div>
+      <h4>Description:</h4>
+      <span className="fa fa-cog"></span>
+    </div>
+    <p>{props.details.description ? props.details.description : "This group does not have description yet."}</p>
   </div>
 );
 
