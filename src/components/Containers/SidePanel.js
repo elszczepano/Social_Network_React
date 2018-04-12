daf236f8bd4474a0086266d9852ed1ee9afec252
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-04-12 17:32:40 +0200

    Rewrite to stateless component

--- src/components/Containers/SidePanel.js
+++ src/components/Containers/SidePanel.js
@@ -1,18 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import AddPostSidePanel from '../Control/AddPostSidePanel';
 import CreateGroupShort from '../Control/CreateGroupShort';
 import '../../assets/scss/main.scss';
 import '../../assets/scss/sidepanel.scss';
 
-class SidePanel extends Component {
-  render() {
-    return (
-      <aside>
-        <CreateGroupShort />
-        <AddPostSidePanel />
-      </aside>
-    );
-  }
-}
+const SidePanel = () => (
+  <aside>
+    <CreateGroupShort />
+    <AddPostSidePanel />
+  </aside>
+);
 
 export default SidePanel;
