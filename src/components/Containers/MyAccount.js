91eb6aa100b8d935a0091aee9f96e3470d165fca
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-04-13 22:00:59 +0200

    Prepare component to add functionality

--- src/components/Containers/MyAccount.js
+++ src/components/Containers/MyAccount.js
@@ -1,36 +1,33 @@
 import React, { Component } from 'react';
 import Header from '../Containers/Header';
+import PropTypes from 'prop-types';
+import API from '../../api.js';
+import { connect } from 'react-redux';
 import Footer from '../View/Footer';
 import '../../assets/scss/main.scss';
 import '../../assets/scss/user/myaccount.scss';
 
 class MyAccount extends Component {
-  user = {
-    firstName: 'John',
-    lastName: 'Doe',
-    birth: '1989-08-20',
-    email: 'johndoe@gmail.com',
-    avatar: 'https://avatarfiles.alphacoders.com/855/85557.png',
-  }
+
   render() {
     return (
       <div>
         <Header />
         <section className="edit-account-contaner default-container">
           <h2 className="text-marker">Edit account details</h2>
-          <p>Edit your personal inforamtion and click <span>update</span></p>
-          <form action="">
+          <p>Edit your personal inforamtion and click <span className="text-marker">update</span></p>
+          <form>
             <div className="edit-account-fields">
               <label htmlFor="firstName">First name</label>
-              <input name="firstName" type="text" defaultValue={this.user.firstName}/>
+              <input name="firstName" type="text" defaultValue={this.props.user.name} />
               <label htmlFor="lastName">Last name</label>
-              <input name="lastNam" type="text" defaultValue={this.user.lastName}/>
+              <input name="lastNam" type="text" defaultValue={this.props.user.surname}/>
               <label htmlFor="birth">Birth date</label>
-              <input name="birth" type="date" defaultValue={this.user.birth}/>
+              <input name="birth" type="date" defaultValue={this.props.user.birth_date}/>
               <label htmlFor="email">E-Mail Address</label>
-              <input name="email" type="email" defaultValue={this.user.email}/>
+              <input name="email" type="email" defaultValue={this.props.user.email}/>
               <label htmlFor="avatar">Avatar</label>
-              <input name="avatar" type="file"/>
+              <input name="avatar" type="file" defaultValue={this.props.user.avatar}/>
               <label htmlFor="password">New password</label>
               <input name="password" type="password"/>
               <label htmlFor="confirmPassword">Confirm password</label>
@@ -48,4 +45,14 @@ class MyAccount extends Component {
   }
 }
 
-export default MyAccount;
+function mapStateToProps(state) {
+  return {
+    user: state.userDetails
+  }
+}
+
+MyAccount.propTypes = {
+  user: PropTypes.object.isRequired
+}
+
+export default connect(mapStateToProps)(MyAccount);
