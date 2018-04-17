aab0daff975dcc442ebc9026baef6ff1249cd4d3
elszczepano <szczepaniak.dominik98@gmail.com>
2018-04-17 09:48:21 +0200

    Remove redundant state

--- src/components/Containers/MyAccount.js
+++ src/components/Containers/MyAccount.js
@@ -13,7 +13,7 @@ import '../../assets/scss/user/myaccount.scss';
 class MyAccount extends Component {
   constructor(props) {
     super(props);
-    this.state = {errMessage: [], user: this.props.user, updated: false};
+    this.state = {errMessage: [], updated: false};
     this.formData = new FormData();
   }
 
@@ -89,15 +89,15 @@ class MyAccount extends Component {
           <form>
             <div className="edit-account-fields">
               <label htmlFor="firstName">First name</label>
-              <input id="name" name="firstName" type="text" onChange={this.handleChange} defaultValue={this.state.user.name} />
+              <input id="name" name="firstName" type="text" onChange={this.handleChange} defaultValue={this.props.user.name} />
               <label htmlFor="lastName">Last name</label>
-              <input id="surname" name="lastName" type="text" onChange={this.handleChange} defaultValue={this.state.user.surname}/>
+              <input id="surname" name="lastName" type="text" onChange={this.handleChange} defaultValue={this.props.user.surname}/>
               <label htmlFor="birth">Birth date</label>
-              <input id="birth_date" name="birth" type="date" onChange={this.handleChange} defaultValue={this.state.user.birth_date}/>
+              <input id="birth_date" name="birth" type="date" onChange={this.handleChange} defaultValue={this.props.user.birth_date}/>
               <label htmlFor="email">E-Mail Address</label>
-              <input id="email" name="email" type="email" onChange={this.handleChange} defaultValue={this.state.user.email}/>
+              <input id="email" name="email" type="email" onChange={this.handleChange} defaultValue={this.props.user.email}/>
               <label htmlFor="avatar">Avatar</label>
-              <input id="avatar" name="avatar" type="file" onChange={this.getAvatar} defaultValue={this.state.user.avatar} accept=".jpg, .jpeg, .png"/>
+              <input id="avatar" name="avatar" type="file" onChange={this.getAvatar} defaultValue={this.props.user.avatar} accept=".jpg, .jpeg, .png"/>
               <label htmlFor="password">New password</label>
                 <div className="eye-handler">
                   <input ref="password" id="password" name="password" onChange={this.handleChange} type="password"/>
