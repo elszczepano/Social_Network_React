16f067a5751ec299d574b46c61946e74c94965df
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-04-27 11:47:45 +0200

    Add scroll to notifications and distinguish unread messages

--- src/components/View/Notification.js
+++ src/components/View/Notification.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import '../../assets/scss/user/notification.scss';
 
-const Notification = (props) => (
-  <div className="notification">
-    <p>{props.content.content}</p>
-    <span className="fa fa-check"></span>
-    <span className="fa fa-times"></span>
-  </div>
-);
+class Notification extends Component {
+  render() {
+    const notificationBackground = classNames({
+    'notification-read': this.props.content.read,
+    'notification': !this.props.content.read
+    });
+    return (
+      <div className={notificationBackground}>
+        <p>{this.props.content.content}</p>
+        <span className="fa fa-check"></span>
+        <span className="fa fa-times"></span>
+      </div>
+    );
+  }
+}
 
 Notification.propTypes = {
   content: PropTypes.object.isRequired
