e7680c63e38069c6e72a7385f5c7fecd39f59c51
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-05-01 12:50:07 +0200

    Handle creating join group requests

--- src/components/Containers/Result.js
+++ src/components/Containers/Result.js
@@ -1,17 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
+import { connect } from 'react-redux';
+import classNames from 'classnames';
+import API from '../../api.js';
 import '../../assets/scss/group/search.scss';
 
-const Result = (props) => (
-  <li className="result">
-    <Link to={`/group/${props.group.id}`}>{props.group.name}</Link>
-    <button>Join</button>
-  </li>
-);
+class Result extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {sent: false}
+  }
+  createNewRequest = () => {
+    API.post('/requests',
+    {
+      group_id: this.props.group.id,
+      user_id: this.props.user.id
+    },
+    {
+      'headers': { 'Authorization': localStorage.getItem("token")}
+    })
+    .then(
+      this.setState({
+      sent: true
+      })
+    )
+    .catch(error => {
+      if(error.response) console.log(error.response['data']['message']);
+      else console.log(error);
+    });
+  }
 
-Result.propTypes = {
-  group: PropTypes.object.isRequired
+  render() {
+    const buttonClass = classNames({
+    'success-button': this.state.sent
+    });
+    return (
+      <li className="result">
+        <Link to={`/group/${this.props.group.id}`}>{this.props.group.name}</Link>
+        <button className={buttonClass} onClick={this.createNewRequest} disabled={this.state.sent}>{this.state.sent ? 'Joined' : 'Join'}</button>
+      </li>
+    )
+  }
 }
 
-export default Result;
+Result.propTypes = {
+  group: PropTypes.object.isRequired,
+  user: PropTypes.object.isRequired
+}
+
+function mapStateToProps(state) {
+  return {
+    user: state.userDetails,
+  }
+}
+
+export default connect(mapStateToProps)(Result);
