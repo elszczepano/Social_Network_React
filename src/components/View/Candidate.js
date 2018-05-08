8e6bc05e4a01fc4c49bdfec079e768eb1addb65a
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-05-08 15:37:16 +0200

    handle approving and deleting requests

--- src/components/View/Candidate.js
+++ src/components/View/Candidate.js
@@ -1,25 +1,67 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import API from '../../api.js';
 import '../../assets/scss/group/members.scss';
 
-class Member extends Component {
+class Candidate extends Component {
   constructor(props) {
     super(props);
-    this.state = {user: this.props.candidate.user.surname ? `${this.props.candidate.user.name} ${this.props.candidate.user.surname}`: this.props.candidate.user.name};
+    this.state = {deleted: false, user: this.props.candidate.user.surname ? `${this.props.candidate.user.name} ${this.props.candidate.user.surname}`: this.props.candidate.user.name};
+  }
+
+  applyRequest = () => {
+    API.post('/user-groups',
+    {
+      group_id: this.props.candidate.group_id,
+      user_id: this.props.candidate.user_id
+    },
+    {
+      'headers': { 'Authorization': localStorage.getItem("token")}
+    })
+    .then(() => {
+      this.deleteRequest();
+    })
+    .catch(error => {
+      if(error.response) console.log(error.response['data']['message']);
+      else console.log(error);
+    });
+  }
+
+  deleteRequest = () => {
+    API.delete(`/requests/${this.props.candidate.id}`, { 'headers': { 'Authorization': localStorage.getItem("token")} })
+    .then(this.setState({
+      deleted: true
+    }))
+    .catch(error => {
+      if(error.response) console.log(error.response['data']['message']);
+      else console.log(error);
+    });
   }
 
   render() {
-    return(
+    const content = !this.state.deleted ? (
       <tr>
         <td>{this.state.user}</td>
         <td className="icon-cell">
-          <span className="fa fa-check"></span>
+          <span onClick={this.applyRequest} className="fa fa-check"></span>
         </td>
         <td className="icon-cell">
-          <span className="fa fa-times"></span>
+          <span onClick={this.deleteRequest} className="fa fa-times"></span>
         </td>
       </tr>
+      ) : (
+        <tr></tr>
+      )
+    return (
+      <React.Fragment>
+        {content}
+      </React.Fragment>
     );
   }
 }
 
-export default Member;
+Candidate.propTypes = {
+  candidate: PropTypes.object.isRequired
+}
+
+export default Candidate;
